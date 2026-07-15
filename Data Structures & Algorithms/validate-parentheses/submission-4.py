4cb6fc7ea576847a1f85606b8e2a8fdcd046c91d
Robert Khzouz <91065437+RobertKhzouz@users.noreply.github.com>
2026-07-14 20:39:05 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        def isClosing(c):
+            closing = True if c in [")", "}", "]"] else False
+            return closing
+
+        if len(s) % 2 != 0:
+            return False
+
+        mapping = {
+            "(" : ")",
+            "{" : "}",
+            "[" : "]"
+        }
+
+        stack = []
+
+        for character in s:
+
+            if not isClosing(character):
+                stack.append(character)
+            else:
+                if len(stack) == 0 or mapping[stack.pop()] != character:
+                    return False
+
+        if len(stack) > 0:
+            return False
+
+        return True
