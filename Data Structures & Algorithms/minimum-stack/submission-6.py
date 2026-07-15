c35e015bc79aebfe9388dcdbdddd413929f4f9b5
Robert Khzouz <91065437+RobertKhzouz@users.noreply.github.com>
2026-07-14 22:24:40 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.prefixes = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.stack) > 1:
+            smaller = len(self.prefixes) if val <= self.stack[self.prefixes[len(self.prefixes) - 1]] else self.prefixes[len(self.prefixes) - 1]
+            self.prefixes.append(smaller)
+        else:
+            self.prefixes.append(0)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.prefixes.pop()
+
+    def top(self) -> int:
+        return self.stack[len(self.stack) - 1]
+
+    def getMin(self) -> int:
+        return self.stack[self.prefixes[len(self.prefixes) - 1]]
